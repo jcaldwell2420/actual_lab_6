80ee90a53db33f051d9e698037e5ac7a6d2bdfcb
Josie Caldwell <josiec04@icloud.com>
2023-03-22 12:41:34 -0400

    created encoder function created run_program while loop and menu

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ def encoder(password):
         encode_int = (int(num) + 3) % 10  # adds 3 to each integer digit, returns a value 0-9 b/c of % 10
         encode_password += str(encode_int)  # converts the encoded integer value to string, added to encode_password
     return encode_password
+
+if __name__ == "__main__":
+    run_program = True
+    encode_pass = None  # stores the encoded password for the decode function
+
+    while run_program:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
